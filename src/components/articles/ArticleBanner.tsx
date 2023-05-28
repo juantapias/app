1124a493126e9be15a51de697ab7cc58ea457aab
Juan Tapias <dev.juantapias@gmail.com>
2023-05-27 22:37:11 -0500

    feat: [APP-010] add details banner

---
+++ src/components/articles/ArticleBanner.tsx
@@ -0,0 +1,28 @@
+import React, { Dispatch, SetStateAction } from 'react'
+
+import { IProduct } from '../../utils/types'
+
+type IArticleBanner = {
+  banner: IProduct
+  dispatchSelectedProduct: Dispatch<SetStateAction<IProduct | undefined>>
+  dispatchDetailProduct: Dispatch<SetStateAction<boolean>>
+}
+
+export default function ArticleBanner ({
+  banner,
+  dispatchSelectedProduct,
+  dispatchDetailProduct
+}: IArticleBanner) {
+  const handleSelectedProduct = () => {
+    dispatchSelectedProduct(banner)
+    dispatchDetailProduct(true)
+  }
+  return (
+    <article
+      className='bg-white h-40 flex items-center justify-center'
+      onClick={handleSelectedProduct}
+    >
+      <div>{banner.title}</div>
+    </article>
+  )
+}
