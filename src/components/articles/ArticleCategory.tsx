d90e370e45399ab32d1db36eca51fef8b6131bee
Juan Tapias <dev.juantapias@gmail.com>
2023-05-28 17:06:03 -0500

    fixup! feat: [APP-012] improvement IProduct type

--- src/components/articles/ArticleCategory.tsx
+++ src/components/articles/ArticleCategory.tsx
@@ -7,5 +7,5 @@ type ArticleCategory = {
 }
 
 export default function ArticleCategory ({ category }: ArticleCategory) {
-  return <div className='flex items-center justify-center bg-white rounded-lg h-32'>{category.title}</div>
+  return <div className='flex items-center justify-center bg-white rounded-lg h-32'>{category.name}</div>
 }
