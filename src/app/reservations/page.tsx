20b7714f07f75524fb9768231ec37a44c4ef36f0
Juan Tapias <dev.juantapias@gmail.com>
2023-09-15 19:56:42 -0500

    feat: [APP-035] refactor booking form

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -1,15 +1,13 @@
-import React, { Fragment } from 'react'
+import { Fragment } from 'react'
 
 import Header from '../../components/shared/Header'
 import Reservation from '../../components/shared/Reservation'
 
-export default function Page () {
+export default function Page() {
   return (
     <Fragment>
       <Header title='Reservas' cartBtn />
-      <main>
-        <Reservation />
-      </main>
+      <Reservation />
     </Fragment>
   )
 }
